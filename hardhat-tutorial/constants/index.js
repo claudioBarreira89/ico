12f1b9f26fe140dd189a3a82343705bc43b8cd27
Cláudio Barreira <claudio.barreira89@gmail.com>
2022-10-13 10:06:17 +0200

    Init ico project

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,5 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS =
+  "0x969D6f4b8f6ed359A3814edCac41e95A9cCdC395";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
